529a7f6c1e82c44f3ae8c791ed577a212f1fceca
Julie Kearns <juliekearns@Julies-MacBook-Air.local>
2020-07-03 19:13:49 -0400

    Shows how flatten and dropRight functions work in their corresponding files

--- dropRight.js
+++ dropRight.js
@@ -1,6 +1,19 @@
-function all(arr) {
-	// Loop through every item in the array
-	// If the item evaluates to false, return false
+function dropRight(arr, n) {
+	// if n is larger than array length return []
+	if (n >= arr.length)
+		return []
 
-	// If the loop ends, return true
+// Use n to count in from the end to get the index
+let rest = (arr.length - n);
+	
+	// return a slice of the array using the index as the end
+	return arr.slice(0, rest)
+
 }
+
+const shoppingList = ['milk', 'eggs', 'bread']
+
+// run a dropRight function for when I pick up the bread, to drop it off my list...
+const revisedShoppingList = dropRight(shoppingList, 1)
+
+console.log(({revisedShoppingList}))
--- flatten.js
+++ flatten.js
@@ -1,9 +1,22 @@
 function flatten(arr) {
 	// Create a new array
+	let newList = []
 
-	// Loop through every item in the array
-	// If the item is an array, add its items to the new array
-	// If not add the item itself to the new array
+// Loop through every item in the array
+	for (let i = 0; i < arr.length; i++){
+// If the item is an array, add its items to the new array
+		if (Array.isArray(arr[i])){
+				newList = newList.concat(arr[i])
 
-	// return the new array
+// If not add the item itself to the new array				
+		} else {
+			newList.push(arr[i])
+		}
+	}
+
+// return the new array
+	return newList
+	 
 }
+
+console.log(flatten([8, [3,6,4], 5, 7]))
