18e2f180ef527bb11fb76d78facd5edbd82b888c
Julie Kearns <juliekearns@Julies-MacBook-Air.local>
2020-07-02 20:17:25 -0400

    Play with functions in xor.js and all.js

--- all.js
+++ all.js
@@ -1,6 +1,12 @@
 function all(arr) {
 	// Loop through every item in the array
+	for (let i = 0; i < arr.length; i++){
+	if(!arr[i]) return false
 	// If the item evaluates to false, return false
-
 	// If the loop ends, return true
 }
+	return true
+}
+
+
+console.log(all([2 === 2, 2 === 2]))
--- xor.js
+++ xor.js
@@ -1,7 +1,14 @@
 function xor(cond1, cond2) {
 	// if cond1 is true and cond2 is false return true
-
+	if(cond1 && !cond2){
+		return true
+	}
 	// if cond1 is false and cond2 is true return true
-
-	// return false
+	if(!cond1 && cond2 ){
+		return true
+	} else {
+		return false
+	}
 }
+
+console.log(xor(2 === 1, 2 === 2))
